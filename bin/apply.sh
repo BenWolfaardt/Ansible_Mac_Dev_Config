bc4ecbe437ff5107cb5f6e2644156b88ff24ec57
Ben Wolfaardt <benjaminwolfaardt@gmail.com>
2025-08-21 00:17:47 +0200

    Update to use uv

--- bin/apply.sh
+++ bin/apply.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-ansible-playbook -i "localhost," -c local main.yml --ask-become-pass
-# TODO: figure out why it keeps asking for the password
-# - Logi Options
-# - Yubico-yubikey
+uv tool run --from ansible-core ansible-playbook -i "localhost," -c local main.yml --ask-become-pass
